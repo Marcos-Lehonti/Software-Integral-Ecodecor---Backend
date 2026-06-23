91fd35a3204e7adc8bb838cab171b9fe5eb7e068
Marcos-Lehonti <marcoslehontiguzmanmontalvan@gmail.com>
2026-06-23 17:12:27 -0400

    Permitir peticiones del frontend en Vercel

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://ecodecor-three.vercel.app'],
   credentials: true
 }));
 
